a097cd0aba741d605e47e8eefd590fec4536f8ec
Paulo Henrique <paulo.henrique06@hotmail.com>
2020-03-29 12:44:01 -0300

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const routes = require('./routes')
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors())
 app.use(routes)
 
 module.exports = app
